5352dd87f7eea7c55107da8e940892c06e70140c
Token196 <89865130+Token196@users.noreply.github.com>
2021-09-08 17:40:45 +0700

    upward

--- js/java.js
+++ js/java.js
@@ -4,3 +4,17 @@ $('.mobile-nav-icon').click(
         $('.menu').slideToggle(200);
     }
 )
+//product-content-bottom button//
+$('.product-content-bottom button').click(
+    function(){
+        $('.product-content-bottom-text').slideToggle(200);
+    }
+)
+//change img//
+const bigImg = document.querySelector(".content-left-big-img img")
+const smalImg = document.querySelectorAll(".content-left-small-img img")
+smalImg.forEach(function(imgItem,X){
+    imgItem.addEventListener("click",function(){
+        bigImg.src = imgItem.src
+    })
+})
